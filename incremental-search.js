e8959d9f4dc495326c3b37cdc0efd25d7f7fd9fd
dunxtand <mdrstandish@gmail.com>
2018-02-16 12:58:35 -0500

    first commit

---
+++ incremental-search.js
@@ -0,0 +1,184 @@
+// this file is for reference and testing; it is not run on the site.
+// a minified version of this file is in libs.js.
+
+window.IncrementalSearch = (function ($, window, document, removeStopWords, undefined) {
+  function isFunction (fn) {
+    return fn && ("function" === typeof fn);
+  }
+
+  var REGEX = {
+    html: /(<([^>]+)>)/ig,
+    punc: /[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,
+    white: /\s+/
+  };
+
+  var MIN_WORD_LENGTH = 3;
+
+  var ITEM_LINK_CLASS = "result-item-link";
+
+  function formatString (str) {
+    return str
+      .replace(REGEX.html, "")
+      .replace(REGEX.punc, "")
+      .toLowerCase();
+  }
+
+  function wordsFrom (input) {
+    var arr = input.val()
+      .replace(REGEX.html, "")
+      .replace(REGEX.punc, "")
+      .toLowerCase()
+      .split(REGEX.white)
+      .filter(function (word) {
+        return "string" === typeof word && word.length >= MIN_WORD_LENGTH;
+      });
+    return removeStopWords(arr);
+  }
+
+  function subwordsFrom (words) {
+    return [].concat.apply([], words.map(function (word) {
+      var subword, subwords = [];
+      for (var i=MIN_WORD_LENGTH; i<word.length; ++i) {
+        subword = word.substring(0, i);
+        if (subword.length >= MIN_WORD_LENGTH) {
+          subwords.push(subword);
+        }
+      }
+      return subwords;
+    }));
+  }
+
+  function createSortedMatchesFor (data) {
+    return function (words, subwords) {
+      return data.filter(function (object) {
+        return object.count(words.concat(subwords)) > 0;
+      }).sort(function (a, b) {
+        return a.count(words) > b.count(words);
+      }).reverse();
+    }
+  }
+
+  function createProto (searchableProps, highlightColor, linkClass) {
+    return {
+      // counts ocurrences of an array of strings
+      count: function (words) {
+        var i, word, match = 0, len = words.length;
+        for (i=0; i<len; ++i) {
+          word = words[i];
+          if (word.length < MIN_WORD_LENGTH) {
+            continue;
+          }
+          searchableProps.forEach(function (propName) {
+            if (!this[propName]) return false;
+            var i2, arr = this[propName];
+            for (i2=0; i2<arr.length; ++i2) {
+              if (arr[i2].indexOf(word) !== -1) {
+                match += 1;
+              }
+            }
+          }, this);
+        }
+        return match;
+      },
+      // returns titled with bolded substrings
+      highlightedTitle (words) {
+        var textArr = this.originalTitle.split(REGEX.white);
+        var opening = "<span style='font-weight:bold;color:" + highlightColor + ";'>",
+            closing = "</span>";
+        var titleWord, searchWord, substr, substrIndex;
+        for (var i1=0; i1<textArr.length; ++i1) {
+          for (var i2=0; i2<words.length; ++i2) {
+            titleWord = textArr[i1].toLowerCase();
+            searchWord = words[i2].toLowerCase();
+            if ((substrIndex = titleWord.indexOf(searchWord)) !== -1) {
+              substr = textArr[i1].substr(substrIndex, searchWord.length);
+              textArr[i1] = textArr[i1].split(substr).join(opening + substr + closing);
+            }
+          }
+        }
+        return textArr.join(" ");
+      },
+      // returns article info as a jQuery <li>
+      asListItem: function (words) {
+        var link = $("<a href='#'></a>");
+        link.attr("href", this.url);
+        link.addClass(linkClass || ITEM_LINK_CLASS);
+        link.html(this.highlightedTitle.call(this, words));
+        return $("<li></li>").addClass("").append(link);
+      }
+    };
+  }
+
+  function createData (initialData, prototype, mapping) {
+    return initialData.map(function (object) {
+      var newObject = Object.assign(
+        Object.create(prototype),
+        object,
+        {
+          originalTitle: object.title,
+          title: formatString(object.title).split(REGEX.white)
+        }
+      );
+      Object.keys(newObject).forEach(function (propName) {
+        if (mapping[propName] && "string" === typeof newObject[propName]) {
+          var newPropName = mapping[propName]["to"] || propName;
+          var delimiter = mapping[propName]["split"];
+          var str = formatString(newObject[propName]);
+          if (mapping[propName]["removeStopWords"] !== false) {
+            str = removeStopWords(str);
+          }
+          newObject[newPropName] = str.split(delimiter || REGEX.white);
+        }
+      });
+      return newObject;
+    });
+  }
+
+  function createInputHandler (input, list, fns) {
+    return function (e) {
+      list.empty();
+      var words = wordsFrom(input);
+      var subwords = subwordsFrom(words);
+      var matches = fns.sortedMatchesFor(words, subwords);
+      if (!words.length || !matches.length) {
+        if (isFunction(fns.onNoMatches)) {
+          fns.onNoMatches(words);
+        }
+        return false;
+      }
+      matches.forEach(function (match) {
+        list.append(match.asListItem(words));
+      });
+      if (isFunction(fns.afterAppend)) {
+        fns.afterAppend(words, matches);
+      }
+    }
+  }
+
+
+  // initialization function
+  return function (settings) {
+    var highlightColor = settings.highlightColor || "#000",
+        mapping = settings.mapping || {};
+    var proto = createProto(settings.searchableProps, highlightColor, settings.linkClass),
+        DATA = createData(settings.data, proto, mapping);
+    var form = settings.form,
+        input = form.find("input"),
+        list = settings.list;
+    var handleInput = createInputHandler(input, list, {
+      sortedMatchesFor: createSortedMatchesFor(DATA),
+      onNoMatches: settings.onNoMatches,
+      afterAppend: settings.afterAppend
+    });
+    input.keyup(handleInput);
+    form.submit(function (e) {
+      e.preventDefault();
+    });
+    if (settings.formFocusin && "function" === typeof settings.formFocusin) {
+      form.focusin(settings.formFocusin);
+    }
+    if (settings.formFocusout && "function" === typeof settings.formFocusout) {
+      form.focusout(settings.formFocusout);
+    }
+  }
+})(jQuery, window, document);
---
+++ readme.md
@@ -0,0 +1,87 @@
+# jQuery Incremental Search
+
+An small jQuery-dependent module that searches an array of objects and adds links to a list as the user types.
+
+### Required Configuration
+
+The text input element must be within a form, and must be the only input element in that form.
+
+The list element must be of type <ul> or <ol>.
+
+### Usage
+
+Link to on page (or just add contents of incremental-search.min.js to page):
+
+````html
+<script src="/path/to/incremental-search.min.js">
+````
+
+Initialize with required and optional arguments:
+
+````javascript
+IncrementalSearch({
+    // REQUIRED
+    data: [
+        // each object must have string properties 'title' and 'link'
+        {title: "A Title of a Page", url: "/path/to/page"},
+        {title: "Another", url: "/an/other"}
+    ],
+    searchableProps: ["title"],
+    form: $("#search-form"), // must be jQuery object
+    list: $("#search-list"), // must be jQuery object
+
+
+    // OPTIONAL
+    highlightColor: "#006cff",
+    linkClass: "class-to-give-the-anchor-elements",
+    onNoMatches: function (words) {
+        // words is array of strings from user input
+    },
+    afterAppend: function (words, matches) {
+        // words is array of string from user input
+        // mathces is array of objects that matched query
+    },
+    formFocusin: function (e) {
+        // called when form's focusin event is emitted
+    },
+    formFocusout: function (e) {
+        // called when form's focusout event is emitted
+    },
+    mapping: { // more about this below
+        added_tags: {
+            to: "tags"
+            split: ", ",
+            removeStopWords: false
+        }
+    }
+});
+````
+
+When matches are found, each match is appended to the list in the following format:
+
+````html
+<li>
+    <a href="/path/to/page" class="class-to-give-the-anchor-elements">
+        A Title of a Page
+    </a>
+</li>
+````
+
+All matches are removed before each new search (before each input 'keyup' event).
+
+### Mapping
+
+Use the 'mapping' argument to tell the module how to configure certain properties of the objects in the data array:
+
+````javascript
+added_tags: { // original name of property
+    to: "tags", // new name of property
+    split: ", ", // how to turn the property into an array (defaults to any amount of whitespace)
+    removeStopWords: false // whether or not to remove common words (defaults to true)
+}
+````
+
+## Notes
+
+* **Removing Stop Words**: You must pass in your own module to remove [stop words](https://en.wikipedia.org/wiki/Stop_words). Pass it into the module as the fourth argument, after $, window, and document. It must be a function that takes a string and returns a new string.
+* **Removing HTML and punctuation**: All searchable properties will be cleared of HTML and punctuation marks.
